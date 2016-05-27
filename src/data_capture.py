d910cf74271b3a8b25021107c21e335bf4beabec
Michael Schaeuble <schaeuble.michael@gmail.com>
2016-05-27 12:36:29 +0200

    Add the target pose to the captured data

--- src/data_capture.py
+++ src/data_capture.py
@@ -3,7 +3,9 @@ import rospy
 import message_filters
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Empty
-from geometry_msgs.msg import TwistStamped
+from geometry_msgs.msg import TwistStamped, PoseStamped
+
+import tf
 
 import csv
 import os
@@ -32,9 +34,10 @@ class DataCapture():
 
         # Synchronized messages
         scan_sub = message_filters.Subscriber('scan', LaserScan)
-        cmd_sub  = message_filters.Subscriber('cmd_vel', TwistStamped)
+        cmd_sub = message_filters.Subscriber('cmd_vel', TwistStamped)
+        target_sub = message_filters.Subscriber('relative_target', PoseStamped)
 
-        self.synchonizer = message_filters.TimeSynchronizer([scan_sub, cmd_sub], 10)
+        self.synchonizer = message_filters.TimeSynchronizer([scan_sub, cmd_sub, target_sub], 10)
         self.synchonizer.registerCallback(self.sync_callback)
 
     def start_callback(self, data):
@@ -55,11 +58,14 @@ class DataCapture():
             self.data_buffer = list()
             self.enable_capture = False
 
-    def sync_callback(self, scan, cmd):
+    def sync_callback(self, scan, cmd, target):
         if self.enable_capture:
             # concatenate the data and add it to the buffer
+            orientation = [target.pose.orientation.x, target.pose.orientation.y,
+                    target.pose.orientation.z, target.pose.orientation.w]
+            yaw = tf.transformations.euler_from_quaternion(orientation)[2]
             new_row = [cmd.header.stamp.to_nsec(), cmd.twist.linear.x, cmd.twist.angular.z] + \
-                    list(scan.ranges)
+                    list(scan.ranges) + [target.pose.position.x, target.pose.position.y, yaw]
             self.data_buffer.append(new_row)
 
     def __write_data_to_file__(self):
@@ -77,7 +83,8 @@ class DataCapture():
 
         # Create the first line of the csv file with column names
         column_line = ['stamp','linear_x','angular_z'] + \
-                ['laser_' + str(i) for i in range(len(self.data_buffer[0]) - 3)]
+                ['laser_' + str(i) for i in range(len(self.data_buffer[0]) - 3)] + ['target_x',
+                        'target_y', 'target_yaw']
 
         # write the data into a csv file and reset the buffer
         with open(os.path.join(self.storage_path,('target_' + str(self.target_count) + '.csv')), \
