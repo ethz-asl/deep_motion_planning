7b83962967fc6548db962d7b6652fc4f89b52ea3
Michael Schaeuble <schaeuble.michael@gmail.com>
2016-06-01 14:46:33 +0200

    Fuse single .csv files into one hdf5 container

--- deep_learning_model/src/data/make_dataset.py
+++ deep_learning_model/src/data/make_dataset.py
@@ -1,16 +1,61 @@
 # -*- coding: utf-8 -*-
 import os
-import click
 import logging
+import argparse
 from dotenv import find_dotenv, load_dotenv
 
+import pandas as pd
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+def parse_args():
+    """Parse input arguments."""
+    parser = argparse.ArgumentParser(description='Fuse csv files and prepare data')
+    parser.add_argument('input_path', help='Folder of the raw data')
+
+    def check_extension(extensions, filename):
+        ext = os.path.splitext(filename)[1]
+        if ext not in extensions:
+            parser.error("Unsupported file extension: Use {}".format(extensions))
+        return filename
+
+    parser.add_argument('output_file', help='Filename of the processed data (stored in \
+            ./data/processed)', type=lambda
+            s:check_extension([".h5"],s))
+    args = parser.parse_args()
+
+    return args
+
+def main(project_dir):
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+
+    args = parse_args()
+
+    path = os.path.join(project_dir, args.input_path)
+    all_files = [os.path.join(path,f) for f in os.listdir(path) 
+                         if os.path.isfile(os.path.join(path, f)) and f.split('.')[-1] == 'csv']
+
+    all_files.sort(key=lambda x: int(os.path.basename(x).split('_')[-1].split('.')[0]))
+
+    target_file = os.path.join(project_dir, 'data', 'processed', args.output_file)
+
+    if os.path.exists(target_file):
+        logger.error('Target file already exists: {}'.format(target_file))
+        exit()
+
+    store = pd.HDFStore(target_file)
+    num_elements = 0
+
+    for i,f in enumerate(all_files):
+        print('({}/{}) {}'.format(i, len(all_files), f), end='\r')
+        current = pd.read_csv(f)
+        num_elements += current.shape[0]
+                            
+        store.append('data', current)
+            
+    print('')
+    store.close()
+    logger.info('We combined {} lines'.format(num_elements))
+    logger.info('File saved: {}'.format(target_file))
 
 
 if __name__ == '__main__':
@@ -24,5 +69,5 @@ if __name__ == '__main__':
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
 
-    main()
+    main(project_dir)
 
