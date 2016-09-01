81f4dda1e36dc0d04843d2ab692c13cc63accdfd
Michael Schaeuble <schaeuble.michael@gmail.com>
2016-09-01 14:13:02 +0200

    Update design of convolutional network

--- deep_learning_model/src/model/conv_model.py
+++ deep_learning_model/src/model/conv_model.py
@@ -6,7 +6,8 @@ in the training script
 import logging
 
 import tensorflow as tf
-from tensorflow.contrib.layers import batch_norm, fully_connected, conv2d
+from tensorflow.contrib.layers import batch_norm, fully_connected, conv2d, xavier_initializer_conv2d, xavier_initializer, l1_regularizer
+import tensorflow.contrib as contrib
 
 # Give the model a descriptive name
 NAME = 'convolutional'
@@ -36,19 +37,40 @@ def inference(data, keep_prob, sample_size, training=True, reuse=False, output_n
     Define the deep neural network used for inference
     """
 
-    laser = tf.slice(data, [0,0], [-1,720])
-    goal = tf.slice(data, [0,720], [-1,-1])
+    # slice 709 elements to get the correct size for the next convolutions
+    laser = tf.slice(data, [0,4], [sample_size,709])
+    goal = tf.slice(data, [0,720], [sample_size,3])
 
-    laser = tf.reshape(laser, [tf.shape(laser)[0], 1, tf.shape(laser)[1], 1])
-    hidden_1 = conv2d(laser, 25, [1,3], reuse=reuse, scope='layer_scope_1')
-    hidden_2 = conv2d(hidden_1, 50, [1,3], reuse=reuse, scope='layer_scope_2')
-    hidden_3 = conv2d(hidden_2, 100, [1,3], reuse=reuse, scope='layer_scope_3')
+    laser = tf.reshape(laser, [sample_size, 1, 709, 1])
+    hidden_1 = conv2d(laser, 64, [1,7], stride=3, normalizer_fn=batch_norm,
+            weights_initializer=xavier_initializer_conv2d(), 
+            weights_regularizer=l1_regularizer(0.001), reuse=reuse, trainable=training, scope='layer_scope_1')
+    hidden_1 = contrib.layers.max_pool2d(hidden_1, [1,3],[1,3], 'SAME')
+    hidden_2 = conv2d(hidden_1, 64, [1,3], normalizer_fn=batch_norm,
+            weights_initializer=xavier_initializer_conv2d(),
+            weights_regularizer=l1_regularizer(0.001), reuse=reuse, trainable=training, scope='layer_scope_2')
+    hidden_3 = conv2d(hidden_2, 64, [1,3], activation_fn=None, normalizer_fn=batch_norm,
+            weights_initializer=xavier_initializer_conv2d(), 
+            weights_regularizer=l1_regularizer(0.001), reuse=reuse, trainable=training, scope='layer_scope_3')
+    hidden_3 = tf.nn.relu(hidden_3 + hidden_1)
+    hidden_4 = conv2d(hidden_3, 64, [1,3], normalizer_fn=batch_norm,
+            weights_initializer=xavier_initializer_conv2d(),
+            weights_regularizer=l1_regularizer(0.001), reuse=reuse, trainable=training, scope='layer_scope_4')
+    hidden_5 = conv2d(hidden_4, 64, [1,3], activation_fn=None, normalizer_fn=batch_norm,
+            weights_initializer=xavier_initializer_conv2d(), 
+            weights_regularizer=l1_regularizer(0.001), reuse=reuse, trainable=training, scope='layer_scope_5')
+    hidden_5 = tf.nn.relu(hidden_5 + hidden_3)
 
-    pooling = tf.nn.avg_pool(hidden_3, [1,1,4,4],[1,1,1,1])
-    pooling = tf.reshape(pooling, [tf.shape(pooling)[0], 180*25])
+    pooling = contrib.layers.avg_pool2d(hidden_5, [1,3],[1,3], 'SAME')
+    pooling = contrib.layers.flatten(pooling)
     combined = tf.concat(1,[pooling, goal])
-    fc_7 = fully_connected(combined, 1024, reuse=reuse, scope='layers_scope_4')
-    prediction = fully_connected(fc_7, CMD_SIZE, activation_fn=None, reuse=reuse, scope='layer_scope_pred')
+    fc_5 = fully_connected(combined, 256, weights_initializer=xavier_initializer(),
+            weights_regularizer=l1_regularizer(0.001), reuse=reuse, trainable=training, scope='fc_scope_5')
+    fc_6 = fully_connected(fc_5, 256, weights_initializer=xavier_initializer(),
+            weights_regularizer=l1_regularizer(0.001), reuse=reuse, trainable=training, scope='fc_scope_6')
+    fc_7 = fully_connected(fc_6, 256, weights_initializer=xavier_initializer(),
+            weights_regularizer=l1_regularizer(0.001), reuse=reuse, trainable=training, scope='fc_scope_7')
+    prediction = fully_connected(fc_7, CMD_SIZE, activation_fn=None, reuse=reuse, trainable=training, scope='layer_scope_pred')
 
     prediction = tf.identity(prediction, name=output_name)
 
